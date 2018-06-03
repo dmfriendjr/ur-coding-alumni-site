b0ebc8ba676428d4735fc6ea481aff8445fb3c8e
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2018-06-03 16:15:57 -0400

    adding public/app/app.js & webpack.mix.js to ignore for easier dev commits

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ let mix = require('laravel-mix');
 
 mix.react('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .browserSync('alumnisite.test');
+    .browserSync('ur-coding-alumni-site.test');
 
