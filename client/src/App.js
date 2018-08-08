dd4c1f4bf4a9308b50739c68424de1a808d06198
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2018-08-08 16:52:21 -0400

    added materialDesign instead of straight bootstrap, everything still all over the place

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Nav from "./components/Nav";
+
 
 const App = () => (
   <Router>
     <div>
-      <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
